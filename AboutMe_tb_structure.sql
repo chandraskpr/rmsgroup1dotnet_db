337f09d50c43430fa58749f12d6e2d2d6113ef31
DevangV <81172044+DevangVashistha@users.noreply.github.com>
2022-03-31 10:46:40 +0530

    Update AboutMe_tb_structure.sql
    Added foreign key userId to table UserResume

--- AboutMe_tb_structure.sql
+++ AboutMe_tb_structure.sql
@@ -2,8 +2,8 @@
 		create table AboutMe(
 		resumeId int primary key ,
 		mainDescription varchar(500) not null,
-		keyPoints varchar(100)/*,foreign key(resumeId)
-		references RMS.() */);
+		keyPoints varchar(100),foreign key(resumeId)
+		references UserId(resumeId) );
 
 		
 
